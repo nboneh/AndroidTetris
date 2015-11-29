ec33703aba18a18b4e7eb2d5aa81240bb8d809c1
Adam <shirong.bai@colorado.edu>
2015-11-29 14:44:45 -0700

    GameThread

--- App/app/src/main/java/com/clouby/tetris/GameFragment.java
+++ App/app/src/main/java/com/clouby/tetris/GameFragment.java
@@ -6,17 +6,39 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * Created by nboneh on 11/15/2015.
- */
-public class GameFragment extends Fragment{
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 
+import android.widget.ImageView;
+
+/**
+ * Created by Shirong on 11/19/2015.
+ */
+public class GameFragment extends Fragment implements View.OnClickListener{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_game, container, false);
+        //pause button
+        (v.findViewById(R.id.pause_button)).setOnClickListener(this);
+
         return v;
     }
+
+
+    @Override
+    public  void onClick(View v){
+        switch(v.getId()){
+            case R.id.pause_button:
+                //TODO change later
+                getActivity().getSupportFragmentManager().popBackStack();
+                break;
+        }
+    }
+
 
 }
--- App/app/src/main/java/com/clouby/tetris/game/GameView.java
+++ App/app/src/main/java/com/clouby/tetris/game/GameView.java
@@ -1,7 +1,62 @@
 package com.clouby.tetris.game;
 
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
 /**
- * Created by nboneh on 11/15/2015.
+ * Created by bunny on 11/29/2015.
  */
-public class GameView {
+public class GameView extends SurfaceView implements SurfaceHolder.Callback{
+    private MainThread thread;
+
+    public GameView(Context context){
+        super(context);
+
+        //add the callback to the surfaceholder to intercept events
+        getHolder().addCallback(this);
+
+        thread = new MainThread(getHolder(), this);
+        //make gamePanel focusable so it can handle events
+        setFocusable(true);
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){}
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder hoder){
+        boolean retry = true;
+
+        while(retry){
+            try {
+                thread.setRunning(false);
+                thread.join();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            retry = false;
+        }
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder){
+        //we can safely start the game loop
+        thread.setRunning(true);
+        thread.start();
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        return super.onTouchEvent(event);
+    }
+
+    public void update(){
+
+
+    }
 }
